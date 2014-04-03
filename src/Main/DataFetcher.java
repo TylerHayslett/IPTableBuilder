938c3ff3116f8d1d17e043dc02a59b4a5ad69e96
TylerHayslett <tylerthe3@gmail.com>
2014-04-03 11:31:25 -0400

    Touched up

--- src/Main/DataFetcher.java
+++ src/Main/DataFetcher.java
@@ -9,7 +9,7 @@ public class DataFetcher {
 			String path = (new File("").getAbsolutePath()) + "\\IPTables.wsf " + ip + " " + usrnm + " " + pass;
 			Runtime.getRuntime().exec("cmd /c start cmd.exe /K \"cd C:\\Users\\student2435 && " + path + "\"");
 			Thread.sleep(1000);
-			//Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
+			Runtime.getRuntime().exec("taskkill /f /im cmd.exe");
 		} catch (IOException e) {} 
 		catch (InterruptedException e) {}
 		
